541265d5411bff98ca4f92387efb9511f1d2c419
LittleBlBb <apdragon2014@gmail.com>
2025-01-24 02:30:19 +0300

    Added CRUD operations for model car

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,33 +1,66 @@
 class CarsController < ApplicationController
+  before_action :set_car!, only: [:show, :edit, :update, :destroy]
+  before_action :set_locations!, only: [:new, :edit]
+  before_action :set_users!, only: [:new, :edit]
+
   def index
-    @cars = Car.all
+    @cars = Car.includes(:location).all
+  end
+
+  def show
   end
 
   def new
     @car = Car.new
   end
 
-  # def create
-  #   @car = Car.new(car_params)
-  # end
-  #
-  # def new
-  #   @car = Car.new
-  # end
-  #
-  # def update
-  #   if @car.update(car_params)
-  #     flash[:success] = 'Car successfully updated!'
-  #     redirect_to car_path
-  #   else
-  #     render :edit
-  #   end
-  # end
-  #
+  def create
+    @car = Car.new(car_params)
+    if @car.save
+      flash[:success] = 'Car created successfully.'
+      redirect_to cars_path
+    else
+      set_locations!
+      set_users!
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @car.update(car_params)
+      flash[:success] = 'Car updated successfully.'
+      redirect_to cars_path
+    else
+      set_locations!
+      set_users!
+      render :edit
+    end
+  end
+
+  def destroy
+    @car.destroy
+    flash[:success] = 'Car deleted successfully.'
+    redirect_to cars_path
+  end
 
   private
 
+  def set_car!
+    @car = Car.find(params[:id])
+  end
+
+  def set_locations!
+    @locations = Location.all
+  end
+
+  def set_users!
+    @users = User.all
+  end
+
   def car_params
-    params.require(:car).permit(:location, :rentals, :users)
+    params.require(:car).permit(:brand, :model, :year, :price_per_day, :location_id)
   end
 end
--- app/models/car.rb
+++ app/models/car.rb
@@ -2,4 +2,9 @@ class Car < ApplicationRecord
   belongs_to :location
   has_many :rentals, dependent: :destroy
   has_many :users, through: :rentals
+  validates :brand, presence: true
+  validates :model, presence: true
+  validates :price_per_day, presence: true
+  validates :year, presence: true, numericality: { only_integer: true }
+  validates :location, presence: true
 end
